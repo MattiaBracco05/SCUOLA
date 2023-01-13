85b86ffdded4b705aaecb7543ada1ba8fdb9c2fa
Mattia Bracco <98776107+MattiaBracco05@users.noreply.github.com>
2023-01-13 10:48:01 +0100

    Update and rename 4C/INFORMATICA/ES_TRADOTTI/tabelline.py to 4C/INFORMATICA/LABORATORIO/ES_TRADOTTI/tabelline.py

--- 4C/INFORMATICA/LABORATORIO/ES_TRADOTTI/tabelline.py
+++ 4C/INFORMATICA/LABORATORIO/ES_TRADOTTI/tabelline.py
@@ -1,4 +1,5 @@
-#Bracco Mattia 4C - Tabelline
+#4C Bracco Mattia - Tabelline C --> py
+
 num = int(input("Inserisci il numero di cui vuoi sapere la tabellina: "))
 for i in range(10):
 	risultato = num * (i + 1)
