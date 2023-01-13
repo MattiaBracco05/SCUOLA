26041b8c834042a1e3113464e2abac5eedf8f9f7
Mattia Bracco <98776107+MattiaBracco05@users.noreply.github.com>
2023-01-13 10:46:49 +0100

    Update and rename 4C/INFORMATICA/ES_TRADOTTI/area_rettangolo.py to 4C/INFORMATICA/LABORATORIO/ES_TRADOTTI/area_rettangolo.py

--- 4C/INFORMATICA/LABORATORIO/ES_TRADOTTI/area_rettangolo.py
+++ 4C/INFORMATICA/LABORATORIO/ES_TRADOTTI/area_rettangolo.py
@@ -1,3 +1,5 @@
+#4C Bracco Mattia - area rettangolo C --> py
+
 base = int(input("Inserisci il valore della base: "))
 altezza = int(input("Inserisci il valore dell'altezza: "))
 area = base * altezza
