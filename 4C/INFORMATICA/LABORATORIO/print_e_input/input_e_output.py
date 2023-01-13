41e5a86496483618ef760b71b5502a2f95e3826e
Mattia Bracco <98776107+MattiaBracco05@users.noreply.github.com>
2023-01-13 10:42:07 +0100

    Update and rename 4C/INFORMATICA/print_e_input/input_e_output.py to 4C/INFORMATICA/LABORATORIO/print_e_input/input_e_output.py

---
+++ 4C/INFORMATICA/LABORATORIO/print_e_input/input_e_output.py
@@ -0,0 +1,5 @@
+#4C Bracco Mattia - Esempi di input e di output
+
+lato = int(input("Inserisci il lato del quadrato: "))
+area = lato ** 2
+print("Il valore dell'area del quadrato di lato", lato, "vale:", area);
