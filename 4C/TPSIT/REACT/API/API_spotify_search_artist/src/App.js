c494073875b76715597edcaa97cf9c3e0996ebbc
Mattia Bracco <98776107+MattiaBracco05@users.noreply.github.com>
2023-01-26 10:14:22 +0100

    Add files via upload

---
+++ 4C/TPSIT/REACT/API/API_spotify_search_artist/src/App.js
@@ -0,0 +1,112 @@
+import {useEffect, useState} from "react";
+import './App.css';
+import axios from 'axios';
+
+function App() {
+    const CLIENT_ID = "46c318244df94bcbb67ef86ce832ee2c"
+    const REDIRECT_URI = "http://localhost:3000"
+    const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+    const RESPONSE_TYPE = "token"
+
+    const [token, setToken] = useState("")
+    const [searchKey, setSearchKey] = useState("")
+    const [artists, setArtists] = useState([])
+
+    useEffect(() => {
+        const hash = window.location.hash
+        let token = window.localStorage.getItem("token")
+
+        if (!token && hash) {
+            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+            window.location.hash = ""
+            window.localStorage.setItem("token", token)
+        }
+
+        setToken(token)
+
+    }, [])
+
+    //LOGOUT --> disconnette e rimanda alla pagina di accesso
+    const logout = () => {
+        setToken("")
+        window.localStorage.removeItem("token")
+    }
+
+    //CERCA ARTISTA --> richiama l'API per la ricerca
+    const cercaArtista = async (e) => {
+        e.preventDefault()
+        const {data} = await axios.get("https://api.spotify.com/v1/search", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            params: {
+                q: searchKey,
+                type: "artist"
+            }
+        })
+
+        setArtists(data.artists.items)
+    }
+
+    //MOSTRA ARTISTA --> informazioni
+    const mostraArtista = () => {
+        console.log(artists)
+        return artists.map(artist => (
+            <div className='row'>
+                {/* immagine */}
+                <div class="datiArtista">{artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div class="col-12">No image<i class="fa fa-sort-amount-desc" aria-hidden="true"></i></div>}</div>
+                {/* nome */}
+                <div class="col-4 datiArtista">Artista: {artist.name}</div>
+                {/* popolarità */}
+                <div class="col-4 datiArtista">Popolarità: {artist.popularity}%</div>
+                {/* followers */}
+                <div class="col-4 datiArtista">Followers: {artist.followers.total}</div>
+                {/* generi (titolo) */}
+                <div class="col-4 linkArtista">Generi:</div>
+                {/* generi (map) */}
+                {artist.genres.map(genere => (
+                    <div class="col-4 datiArtista">·{genere}</div>
+                ))}
+                {/* link */}
+                <div class="col-4 linkArtista"><a href={artist.external_urls.spotify} target="new">VISUALIZZA IN APP</a></div>
+            </div>
+        ))
+    }
+
+//     <div key={artist.id}>
+    //     {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div>No image <i class="fa fa-sort-amount-desc" aria-hidden="true"></i></div>}
+    //     {artist.name}
+    //     {artist}
+//      </div>
+
+    //RETURN --> STAMPA A VIDEO
+    return (
+        <div className="App">
+            <header className="App-header">
+                {/* titolo */}
+                <h1>React JS ~ Spotify Artists Search</h1>
+
+                {/* BUTTON PER IL LOGUT */}
+                {!token ?
+                    <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+                    : <button className="buttonLogout" onClick={logout}>LOGOUT</button>}
+
+                {/* INPUT PER LA RICERCA */}
+                {token ?
+                    <form onSubmit={cercaArtista}>
+                        {/* casella di testo */}
+                        <input type="text" className="textCerca" onChange={e => setSearchKey(e.target.value)}/>
+                        {/* button */}
+                        <button type={"submit"} className="buttonSearch" >SEARCH</button>
+                    </form>
+                    : <h2>EFFETTUARE IL LOGIN</h2>
+                }
+
+                {mostraArtista()}
+
+            </header>
+        </div>
+    );
+}
+
+export default App;
