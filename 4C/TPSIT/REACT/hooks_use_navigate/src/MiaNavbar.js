a89bb4bd0551748020782accc445f480ff43e3f7
Mattia Bracco <98776107+MattiaBracco05@users.noreply.github.com>
2023-02-15 21:04:59 +0100

    Add files via upload

---
+++ 4C/TPSIT/REACT/hooks_use_navigate/src/MiaNavbar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {NavLink, useNavigate} from "react-router-dom"
+
+const MiaNavbar = () => {
+
+    const pippo = useNavigate()
+
+    //FUNZIONE APRI (richiamata alla pressione del button)
+    const apri =() => {
+        console.log("FUNZIONE APRI (pressione del button)")
+        pippo("/ChiSiamo")
+    }
+
+    //RETURN --> Stampa a video    
+    return (
+        <div>
+            {/* Navbar */}
+            <NavLink to={"/"}>HOME</NavLink><br/>
+            <NavLink to={"/ChiSiamo"}>Chi siamo ?</NavLink><br/>
+            <NavLink to={"/DoveSiamo"}>Dove siamo ?</NavLink><br/>
+
+            {/* Equivalente del cliccare la fraccie indietro (<--) del browser, torna al componente aperto precedentemente */}
+            <NavLink to={-1}>INDIETRO</NavLink><br/>
+
+            {/* Button */}
+            <input type="button" value="API" onClick={apri}/>
+        </div>
+    )
+}
+
+export default MiaNavbar
