eeeddd581d068c93beef95c200aaa42060afe3de
Mattia Bracco <98776107+MattiaBracco05@users.noreply.github.com>
2023-03-02 10:27:51 +0100

    Add files via upload

---
+++ 4C/TPSIT/REACT/ricerca_noleggio/src/CercaPerCognome.js
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from 'react'
+
+const URL = "https://raw.githubusercontent.com/andgio1976/autonoleggio/main/dati"
+let mieiVeicoli = new Array()
+
+const CercaPerCognome = () => {
+
+    //USE STATE
+    const [nomeRicerca, setNomeRicerca] = useState('');
+    const [veicoli, setVeicoli] = useState([])
+    const [risultato, setRisulatato] = useState([])
+
+    //USE EFFECT PER LA CONNESSIONE
+    useEffect(() => {
+        connessione();
+        }, [])
+
+    const connessione =()=> {
+        console.log("CONNESSIONE FILE JSON")
+        //FETCH
+        fetch(URL)
+            .then((testo) => testo.json())
+            .then((mioJSON) => {
+		    
+                //COMANDO MAP --> ciclo per il numero di città presenti nel file JSON
+                mioJSON.map((element, index) => {
+                    mieiVeicoli.push(element)
+                })
+            })
+      }
+      
+    //FUNZIONE alertNome (RICHIAMATA ALLA PRESSIONE DEL BUTTON CONFERMA)
+    const alertNome = (event) => {
+        event.preventDefault();
+        setRisulatato([])
+    
+        //Ciclo per i veivoli
+        let flag = 0
+
+        for (var i=0; i<8; i++) {
+            console.log("*****Sono dentro al ciclo for*****")
+
+            //Controllo se la targa corrisponde --> se corrisponde stampo i dati
+            if (nomeRicerca == mieiVeicoli[i].cliente.cognome) {
+                console.log("trovato")
+                flag = 1;
+                var prezzo = mieiVeicoli[i].tariffa * mieiVeicoli[i].cliente.giorni
+                setRisulatato([...risultato,
+                <div class="row justify-content-center mt-3">
+                    <div class="col-12">Nome: {mieiVeicoli[i].cliente.nome} Cognome: {mieiVeicoli[i].cliente.cognome} Anno: {mieiVeicoli[i].cliente.anno}</div>
+                    <div class="col-12">Marca: {mieiVeicoli[i].marca} Modello: {mieiVeicoli[i].modello} Tariffa: {mieiVeicoli[i].tariffa}</div>
+                    <div class="col-12">Prezzo noleggio: {prezzo}€</div>
+                </div>])
+            }
+        }
+
+        //Se il cognome non è stato trovato --> messaggio di errore
+        if (flag == 0) {
+            setRisulatato([
+                <div class="row justify-content-center mt-3">
+                    <div class="col-12">Utente non trovato</div>
+                </div>])
+        }
+
+    }
+
+    //RETURN --> Stampa a video
+    return (
+        <div>
+            <h1>Cerca per cognome</h1>
+            {/* FORM */}
+            <form onSubmit={alertNome}>
+            
+                {/* INPUT TEXT */}
+                <label>
+                    <input type="text" value={nomeRicerca} placeholder={"Inserisci un cognome"} onChange={(e) => setNomeRicerca(e.target.value)} />
+                </label>
+                {/* BUTTON PER CONFERMARE */}
+                <input type="submit" value={"CONFERMA"} />
+
+                {/* RISULTATO */}
+                {risultato}
+                
+            </form>
+        </div>
+  )
+}
+
+export default CercaPerCognome
