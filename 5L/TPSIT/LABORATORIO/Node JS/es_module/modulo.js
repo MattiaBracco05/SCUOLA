a0643f5b2d925c6db9bed66661bce5fbfd1e4121
Mattia Bracco <98776107+MattiaBracco05@users.noreply.github.com>
2023-11-16 13:27:41 +0100

    Add files via upload

---
+++ 5L/TPSIT/LABORATORIO/Node JS/es_module/modulo.js
@@ -0,0 +1,17 @@
+//let pippo = 0 //Variabile locale al modulo
+
+//Funzione SOMMA
+export const somma=(num1, num2) => {
+    let totale = num1 + num2
+    return (totale)
+}
+
+//Funzione DIFFERENZA
+export const differenza=(num1, num2) => {
+    let totale = num1 - num2
+    return (totale)
+}
+
+/*
+Per esportare le mie funzioni inserisco "export" prima di "const" dove dichiaro le funzioni
+*/
